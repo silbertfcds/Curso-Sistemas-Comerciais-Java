f52bd03d99d0028f2b7baa2c03dca53afa14ae23
silbertfcds <silbert.fcds@hotmail.com>
2015-09-04 12:21:11 -0300

    Desenvolvimento da telas: cliente, pedido e usuario

---
+++ PedidoVenda/src/main/java/firmino/silbert/controller/PesquisaClientesBean.java
@@ -0,0 +1,25 @@
+package firmino.silbert.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class PesquisaClientesBean {
+
+	private List<Integer> clientesFiltrados;
+	
+	public PesquisaClientesBean() {
+		clientesFiltrados = new ArrayList<>();
+		for (int i = 0; i < 50; i++) {
+			clientesFiltrados.add(i);
+		}
+	}
+
+	public List<Integer> getClientesFiltrados() {
+		return clientesFiltrados;
+	}
+}
